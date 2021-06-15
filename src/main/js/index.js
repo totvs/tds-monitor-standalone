343988d9d0f32efce6ebf9f0a0c0b7fa700af70a
Alan Cândido <brodao@gmail.com>
2021-06-15 14:38:30 -0300

    Ajuste no processo de instânciamento do cliente LS.

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,22 +1,23 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-const argv = require('yargs') // eslint-disable-line
-    .options('logging', {
-        alias: ['l'],
-        describe: 'Enable LS logging',
-        type: 'boolean',
-        default: false
-    })
-    .options('dev', {
-        alias: ['d'],
-        describe: 'Open with DevTools',
-        type: 'boolean',
-        default: false
-    })
-    .argv;
-require('@totvs/tds-languageclient').TdsLanguageClient.instance({ logging: argv.logging });
+const argv = require("yargs") // eslint-disable-line
+	.options("logging", {
+		alias: ["l"],
+		describe: "Enable LS logging",
+		type: "boolean",
+		default: false,
+	})
+	.options("dev", {
+		alias: ["d"],
+		describe: "Open with DevTools",
+		type: "boolean",
+		default: false,
+	}).argv;
+require("@totvs/tds-languageclient").TdsLanguageClient.instance({
+	logging: argv.logging,
+});
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -25,17 +26,22 @@ let mainWindow;
 function createWindow() {
 	// Create the browser window.
 	mainWindow = new BrowserWindow({
+		show: false,
 		width: 800,
 		height: 600,
-		title: 'TOTVS Monitor',
-		icon: path.join(__dirname, '..', 'resources', 'icons', '1024x1024.png'),
+		title: "TOTVS Monitor",
+		icon: path.join(__dirname, "..", "resources", "icons", "1024x1024.png"),
 
 		//titleBarStyle: "hidden",
 		frame: false,
 		webPreferences: {
 			nodeIntegration: true,
-			preload: path.join(__dirname, 'preload.js')
+			preload: path.join(__dirname, "preload.js"),
+			additionalArguments: [ argv.logging?"--logging":""]
 		}
+	});
+	mainWindow.once("ready-to-show", () => {
+		mainWindow.show();
 	});
 
 	// Para remover o menu:
@@ -49,18 +55,18 @@ function createWindow() {
 
 	*/
 
-	mainWindow.loadFile(require.resolve('@totvs/tds-monitor-frontend'));
+	mainWindow.loadFile(require.resolve("@totvs/tds-monitor-frontend"));
 
 	// and load the index.html of the app.
 	//mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
 	// Open the DevTools.
 	if (argv.dev) {
-		mainWindow.webContents.openDevTools()
+		mainWindow.webContents.openDevTools();
 	}
 
 	// Emitted when the window is closed.
-	mainWindow.on('closed', function() {
+	mainWindow.on("closed", function () {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
@@ -71,21 +77,19 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on("window-all-closed", function () {
 	// On macOS it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin')
-		app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
 
-app.on('activate', function() {
+app.on("activate", function () {
 	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null)
-		createWindow();
+	if (mainWindow === null) createWindow();
 });
 
 // In this file you can include the rest of your app's specific main process
