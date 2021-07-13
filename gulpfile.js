b7baf4fa9369869063e4630a5cd5145fe7158c28
Alan Cândido <brodao@gmail.com>
2021-07-13 16:25:05 -0300

    Arquivos de tradução com 'marca'.

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,47 @@
-'use strict';
+"use strict";
 
-const nls = require("vscode-nls-dev");
+let gulp = require("gulp"),
+	shelljs = require("shelljs"),
+	path = require("path"),
+	yargs = require("yargs");
 
-let gulp = require('gulp'),
-	shelljs = require('shelljs'),
-	path = require('path'),
-	yargs = require('yargs');
-
-let tasksDir = path.join(__dirname, 'src', 'tasks'),
+let tasksDir = path.join(__dirname, "src", "tasks"),
 	tasks = shelljs.ls(tasksDir),
 	plugins = {},
 	argv = yargs
-		.options('x64', {
-			alias: ['6', '64', 'amd64'],
-			describe: 'Aciona build 64 bits',
-			type: 'boolean',
-			default: false
+		.options("x64", {
+			alias: ["6", "64", "amd64"],
+			describe: "Aciona build 64 bits",
+			type: "boolean",
+			default: false,
 		})
-		.options('x86', {
-			alias: ['3', '32', 'x32', 'win32', 'ia32'],
-			describe: 'Aciona build 32 bits',
-			type: 'boolean',
-			default: false
+		.options("x86", {
+			alias: ["3", "32", "x32", "win32", "ia32"],
+			describe: "Aciona build 32 bits",
+			type: "boolean",
+			default: false,
 		})
-		.options('windows', {
-			alias: ['w', 'win'],
-			describe: 'Aciona build Windows',
-			type: 'boolean',
-			default: false
+		.options("windows", {
+			alias: ["w", "win"],
+			describe: "Aciona build Windows",
+			type: "boolean",
+			default: false,
 		})
-		.options('linux', {
-			alias: ['l', 'lin'],
-			describe: 'Aciona build Linux',
-			type: 'boolean',
-			default: false
+		.options("linux", {
+			alias: ["l", "lin"],
+			describe: "Aciona build Linux",
+			type: "boolean",
+			default: false,
 		})
-		.options('mac', {
-			alias: ['m', 'osx', 'macosx'],
-			describe: 'Aciona build MacOS',
-			type: 'boolean',
-			default: false
-		})
-		.argv;
+		.options("mac", {
+			alias: ["m", "osx", "macosx"],
+			describe: "Aciona build MacOS",
+			type: "boolean",
+			default: false,
+		}).argv;
 
 tasks.forEach((filename) => {
-	let taskname = path.basename(filename, '.js'),
+	let taskname = path.basename(filename, ".js"),
 		taskfile = path.join(tasksDir, filename),
 		taskaction = require(taskfile)(gulp, plugins, __dirname, argv);
 
